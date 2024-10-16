8d02a88e1550843908f3e4f09a7be6977401cac4
Minhajuddin Khaja <minhajuddin.k@gmail.com>
2024-10-15 22:57:55 -0400

    db persistence

--- app.go
+++ app.go
@@ -4,25 +4,71 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"time"
+	"strings"
+
+	"github.com/julienschmidt/httprouter"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 )
 
-type requestHandler struct{}
-
-func (h *requestHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	log.Printf("%s\t[%s]\t%s\n", r.RemoteAddr, r.Method, r.URL.Path)
-	fmt.Fprintf(w, "<!doctype html><h1>Welcome to the thunderdome!</h1>")
+type Login struct {
+	ID    int64  `db:"id"`
+	Email string `db:"email"`
 }
 
-func main() {
-	log.Println("Starting Multi Page TODO")
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	var err error
+	w.Header().Add("content-type", "text/html")
+	b := strings.Builder{}
+	logins := []Login{}
+	err = db.Select(&logins, "SELECT id, email From logins")
+	log.Printf("ERROR: %v\n", err)
+	b.WriteString("<ul>")
+	for i, l := range logins {
+		fmt.Fprintf(w, "<li>%d %d %s</li>", i+1, l.ID, l.Email)
+	}
+	b.WriteString("</ul>")
+	fmt.Fprintf(w, `<doctype html>
+		%s
+		<form method=post action="/todo/">
+		<input type=text name=title />
+		<button type=submit>Save</button>
+		</form>
+		`, b.String())
+}
 
-	server := &http.Server{
-		Addr:         ":8080",
-		Handler:      &requestHandler{},
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 10 * time.Second,
+func CreateTodo(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	err := r.ParseForm()
+	log.Printf("ERROR Parsing %v\n", err)
+	log.Printf("%v", r.PostForm)
+	if title, ok := r.PostForm["title"]; ok {
+		log.Printf("Creating task '%s'\n", title[0])
+		tx := db.MustBegin()
+		tx.MustExec("INSERT INTO logins (email) VALUES($1)", title[0])
+		tx.Commit()
+	}
+	w.Header().Add("location", "/?info=task-created")
+	w.WriteHeader(302)
+}
+
+var db *sqlx.DB
+
+func main() {
+	var err error
+	db, err = sqlx.Connect("postgres", "user=postgres password=postgres dbname=minhajuddin sslmode=disable")
+	if err != nil {
+		log.Fatalln(err)
 	}
 
-	log.Fatal(server.ListenAndServe())
+	logins := []Login{}
+	err = db.Select(&logins, "SELECT id, email From logins")
+	log.Printf("ERROR: %v\n", err)
+	fmt.Println("LOGINS", logins)
+
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.POST("/todo", CreateTodo)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/minhajuddin/mptodo
 
 go 1.22.4
+
+require (
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/lib/pq v1.10.9
+)
+
+require github.com/jmoiron/sqlx v1.4.0
